a66e5c8431590a4fcebb02825371dfd5a2c1f851
Talipov Timurlan <taltima4@gmail.com>
2026-04-24 14:38:06 +0600

    Создал контроллер для артиста,сделал схему,и и заинжектил модель для работы с монгусом,добавил в контролле возможность получения всех артистов,конкретного,удаления конкретного артиста и добавления артиста с возможностью передачи картинки.

---
+++ src/artists/artists.controller.ts
@@ -0,0 +1,66 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post, UploadedFile,
+  UseInterceptors
+} from '@nestjs/common';
+import {InjectModel} from "@nestjs/mongoose";
+import {Artist, ArtistDocument} from "../schemas/artist.schema";
+import {Model} from "mongoose";
+import {CreateArtistDto} from "./create-artist.dto";
+import {FileInterceptor} from "@nestjs/platform-express";
+import path from "node:path";
+import {promises as fs} from "node:fs";
+import {randomUUID} from "node:crypto";
+import {diskStorage} from "multer";
+
+@Controller('artists')
+export class ArtistsController {
+
+  constructor(
+    @InjectModel(Artist.name)
+    private artistModel: Model<ArtistDocument>
+  ) {
+  }
+
+  @Get()
+  async getAll() {
+    return this.artistModel.find();
+  }
+
+  @Get(':id')
+  async getOne(@Param('id') id: string) {
+    return this.artistModel.findById(id)
+  }
+
+  @Post()
+  @UseInterceptors(
+    FileInterceptor('photo', {
+      storage: diskStorage({
+        destination: './public/uploads/artists',
+        filename: async (_req, file, cb) => {
+          const extension = path.extname(file.originalname);
+          cb(null, randomUUID() + extension);
+        }
+      })
+    }),
+  )
+  async create(
+    @UploadedFile() file: Express.Multer.File,
+    @Body() artistDto: CreateArtistDto) {
+    const newArtist = new this.artistModel({
+      name: artistDto.name,
+      photo: file ? 'uploads/artists/' + file.filename : null,
+      information: artistDto.information,
+    })
+    return newArtist.save()
+  }
+
+  @Delete(":id")
+  async deleteOne(@Param('id') id: string) {
+    return this.artistModel.findByIdAndDelete(id);
+  }
+}
---
+++ src/schemas/artist.schema.ts
@@ -0,0 +1,22 @@
+import {Prop, Schema, SchemaFactory} from "@nestjs/mongoose";
+import { Document } from 'mongoose';
+
+export type ArtistDocument = Artist & Document;
+
+@Schema()
+export class Artist {
+  @Prop({required: true})
+  name: string;
+
+  @Prop({default: null})
+  information: string;
+
+  @Prop({default: false})
+  isPublished: boolean;
+
+  @Prop({default: null})
+  photo: string;
+}
+
+
+export const ArtistSchema = SchemaFactory.createForClass(Artist);
