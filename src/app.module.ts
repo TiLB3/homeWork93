59775d0f881b10f9cfedce174ebd5fec66ec5e20
Talipov Timurlan <taltima4@gmail.com>
2026-04-27 13:11:25 +0600

    [hw 94] Создал схему для юзера

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,33 @@
-import {Module} from '@nestjs/common';
-import {AppController} from './app.controller';
-import {AppService} from './app.service';
-import {ArtistsController} from './artists/artists.controller';
-import {MongooseModule} from "@nestjs/mongoose";
-import {Artist, ArtistSchema} from "./schemas/artist.schema";
-import {AlbumsController} from './albums/albums.controller';
-import {Album, AlbumSchema} from "./schemas/album.schema";
-import {TracksController} from './tracks/tracks.controller';
-import {Track, TrackSchema} from "./schemas/tracks.schema";
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ArtistsController } from './artists/artists.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Artist, ArtistSchema } from './schemas/artist.schema';
+import { AlbumsController } from './albums/albums.controller';
+import { Album, AlbumSchema } from './schemas/album.schema';
+import { TracksController } from './tracks/tracks.controller';
+import { Track, TrackSchema } from './schemas/tracks.schema';
+import { User, UserSchema } from './schemas/user.schema';
+import { UsersController } from './users/users.controller';
 
 @Module({
   imports: [
-    MongooseModule.forRoot("mongodb://localhost/spotify-Timur"),
+    MongooseModule.forRoot('mongodb://localhost/spotify-Timur'),
     MongooseModule.forFeature([
-      {name: Artist.name, schema: ArtistSchema},
-      {name: Album.name, schema: AlbumSchema},
-      {name: Track.name, schema: TrackSchema},
-    ])
+      { name: Artist.name, schema: ArtistSchema },
+      { name: Album.name, schema: AlbumSchema },
+      { name: Track.name, schema: TrackSchema },
+      { name: User.name, schema: UserSchema },
+    ]),
   ],
-  controllers: [AppController, ArtistsController, AlbumsController, TracksController],
+  controllers: [
+    AppController,
+    ArtistsController,
+    AlbumsController,
+    TracksController,
+    UsersController,
+  ],
   providers: [AppService],
 })
-export class AppModule {
-}
+export class AppModule {}
---
+++ src/schemas/user.schema.ts
@@ -0,0 +1,60 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { randomUUID } from 'node:crypto';
+import bcrypt from 'bcrypt';
+import { Document } from 'mongoose';
+
+interface UserMethods {
+  generateToken: () => void;
+  checkPassword: () => boolean;
+}
+
+const SALT_WORK_FACTOR = 10;
+
+export type UserDocument = User & Document & UserMethods;
+
+@Schema()
+export class User {
+  @Prop({
+    required: true,
+    unique: true,
+  })
+  username: string;
+  @Prop({
+    required: true,
+  })
+  password: string;
+  @Prop({
+    required: true,
+  })
+  token: string;
+  @Prop()
+  displayName: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
+
+UserSchema.methods.generateToken = function (this: UserDocument) {
+  this.token = randomUUID();
+};
+
+UserSchema.methods.checkPassword = function (
+  this: UserDocument,
+  password: string,
+) {
+  return bcrypt.compare(password, this.password);
+};
+UserSchema.pre<UserDocument>('save', async function () {
+  if (!this.isModified('password')) return;
+
+  const salt = await bcrypt.genSalt(SALT_WORK_FACTOR);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+UserSchema.set('toJSON', {
+  transform: (doc, ret) => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { password, token, ...rest } = ret;
+
+    return rest;
+  },
+});
